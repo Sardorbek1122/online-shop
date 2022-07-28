d90a40a0982f7234988e26f5979a855ed922d319
Sardorbek <smakhamadaliev8@gmail.com>
2022-07-28 23:56:00 +0200

    added remove function

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -2,8 +2,10 @@ import './Card.scss';
 import React from 'react';
 import { formatCurrency } from '../../Use/formatCurrency';
 import 'bulma';
+import { useShoppingCart } from '../../Context/ShoppingCartContext';
 
 type Props = {
+  id: number;
   title: string;
   price: number;
   description: string;
@@ -12,10 +14,16 @@ type Props = {
 
 export const Card: React.FC<Props> = (
   {
-    title, price, description, image,
+    id, title, price, description, image,
   }:Props,
 ) => {
-  const quantity = 1;
+  const {
+    getItemQuentity,
+    increaseCartQuentity,
+    decreaseCartQuentity,
+    removeFromCart,
+  } = useShoppingCart();
+  const quantity = getItemQuentity(id);
 
   return (
     <div className="card">
@@ -25,18 +33,38 @@ export const Card: React.FC<Props> = (
       <p>{description}</p>
 
       {quantity === 1 ? (
-        <button type="button">+ Add</button>
+        <button
+          type="button"
+          onClick={() => increaseCartQuentity(id)}
+        >
+          + Add
+        </button>
       ) : (
         <div>
           <div>
-            <button type="button">-</button>
+            <button
+              type="button"
+              onClick={() => decreaseCartQuentity(id)}
+            >
+              -
+            </button>
             <div>
               <span>{quantity}</span>
               in cart
             </div>
-            <button type="button">+</button>
+            <button
+              type="button"
+              onClick={() => increaseCartQuentity(id)}
+            >
+              +
+            </button>
           </div>
-          <button type="button">Remove</button>
+          <button
+            type="button"
+            onClick={() => removeFromCart(id)}
+          >
+            Remove
+          </button>
         </div>
       )}
     </div>
--- src/Context/ShoppingCartContext.tsx
+++ src/Context/ShoppingCartContext.tsx
@@ -1,18 +1,85 @@
-import { createContext, useContext, ReactNode } from 'react';
+import {
+  createContext,
+  useContext,
+  ReactNode,
+  useState,
+} from 'react';
 
 type ShoppingCartProviderProps = {
   children: ReactNode
 };
 
-const ShoppingCartContext = createContext({});
+type ShoppingCartContextType = {
+  getItemQuentity: (id: number) => number;
+  increaseCartQuentity: (id: number) => void;
+  decreaseCartQuentity: (id: number) => void;
+  removeFromCart: (id: number) => void;
+};
+
+type CartItem = {
+  id: number,
+  quantity: number,
+};
+
+const ShoppingCartContext = createContext({} as ShoppingCartContextType);
 
 export function useShoppingCart() {
   return useContext(ShoppingCartContext);
 }
 
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  function getItemQuentity(id: number) {
+    return cartItems.find(item => item.id === id)?.quantity || 0;
+  }
+
+  function increaseCartQuentity(id: number) {
+    setCartItems(currItems => {
+      if (currItems.find(item => item.id === id) == null) {
+        return [...currItems, { id, quantity: 1 }];
+      }
+
+      return currItems.map(item => {
+        if (item.id === id) {
+          return { ...item, quentity: item.quantity + 1 };
+        }
+
+        return item;
+      });
+    });
+  }
+
+  function decreaseCartQuentity(id: number) {
+    setCartItems(currItems => {
+      if (currItems.find(item => item.id === id)?.quantity === 1) {
+        return currItems.filter(item => item.id !== id);
+      }
+
+      return currItems.map(item => {
+        if (item.id === id) {
+          return { ...item, quentity: item.quantity - 1 };
+        }
+
+        return item;
+      });
+    });
+  }
+
+  function removeFromCart(id: number) {
+    setCartItems(currItems => {
+      return currItems.filter(item => item.id !== id);
+    });
+  }
+
   return (
-    <ShoppingCartContext.Provider value={{}}>
+    <ShoppingCartContext.Provider value={{
+      getItemQuentity,
+      increaseCartQuentity,
+      decreaseCartQuentity,
+      removeFromCart,
+    }}
+    >
       {children}
     </ShoppingCartContext.Provider>
   );
